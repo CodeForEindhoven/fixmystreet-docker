435bbc1836205af5a3ec047c04a79b633c66e2e6
Milo van der Linden <milo@dogodigi.net>
2015-12-03 10:30:21 +0100

    Added ENV sed

--- run.sh
+++ run.sh
@@ -3,7 +3,34 @@
 #psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
 #psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
 
+sed -i "s/__DB_HOST__/$PG_HOST/g" /fixmystreet/conf/general.yml
+sed -i "s/__DB_PORT__/$PG_PORT/g" /fixmystreet/conf/general.yml
+sed -i "s/__DB_NAME__/$PG_DATABASE/g" /fixmystreet/conf/general.yml
+sed -i "s/__DB_USER__/$PG_USER/g" /fixmystreet/conf/general.yml
+sed -i "s/__DB_PASS__/$PG_PASSWORD/g" /fixmystreet/conf/general.yml
+sed -i "s/__BASE_URL__/$BASE_URL/g" /fixmystreet/conf/general.yml
+sed -i "s/__EMAIL_DOMAIN__/$EMAIL_DOMAIN/g" /fixmystreet/conf/general.yml
+sed -i "s/__CONTACT_EMAIL__/$CONTACT_EMAIL/g" /fixmystreet/conf/general.yml
+sed -i "s/__CONTACT_NAME__/$CONTACT_NAME/g" /fixmystreet/conf/general.yml
+sed -i "s/__NOREPLY_EMAIL__/$NOREPLY_EMAIL/g" /fixmystreet/conf/general.yml
+sed -i "s/__STREET_EXAMPLE1__/$STREET_EXAMPLE1/g" /fixmystreet/conf/general.yml
+sed -i "s/__STREET_EXAMPLE2__/$STREET_EXAMPLE2/g" /fixmystreet/conf/general.yml
+sed -i "s/__APP_LANGUAGES__/$LANGUAGES/g" /fixmystreet/conf/general.yml
+sed -i "s/__MAPIT_URL__/$MAPIT_URL/g" /fixmystreet/conf/general.yml
+sed -i "s/__MAPIT_TYPES__/$MAPIT_TYPES/g" /fixmystreet/conf/general.yml
+sed -i "s/__MAPIT_ID_WHITELIST__/$MAPIT_ID_WHITELIST/g" /fixmystreet/conf/general.yml
+sed -i "s/__MAPIT_GENERATION__/$MAPIT_GENERATION/g" /fixmystreet/conf/general.yml
+sed -i "s/__SMTP_SMARTHOST__/$SMTP_SMARTHOST/g" /fixmystreet/conf/general.yml
+sed -i "s/__SMTP_TYPE__/$SMTP_TYPE/g" /fixmystreet/conf/general.yml
+sed -i "s/__SMTP_PORT__/$SMTP_PORT/g" /fixmystreet/conf/general.yml
+sed -i "s/__SMTP_USERNAME__/$SMTP_USERNAME/g" /fixmystreet/conf/general.yml
+sed -i "s/__SMTP_PASSWORD__/$SMTP_PASSWORD/g" /fixmystreet/conf/general.yml
+sed -i "s/__GAZE_URL__/$GAZE_URL/g" /fixmystreet/conf/general.yml
+sed -i "s/__AREA_LINKS_FROM_PROBLEMS__/$AREA_LINKS_FROM_PROBLEMS/g" /fixmystreet/conf/general.yml
+sed -i "s/__TESTING_COUNCILS__/$TESTING_COUNCILS/g" /fixmystreet/conf/general.yml
+sed -i "s/__MESSAGE_MANAGER_URL__/$MESSAGE_MANAGER_URL/g" /fixmystreet/conf/general.yml
+
 # reports
 /fixmystreet/bin/update-all-reports
-
-script/fixmystreet_app_server.pl -d --fork
+# run
+/fixmystreet/script/fixmystreet_app_server.pl -d --fork
