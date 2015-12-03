aa7b72b4c6a89c8c0ae277ba70e5b60538ecdd66
Milo van der Linden <milo@dogodigi.net>
2015-12-03 08:52:34 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
+#! /bin/bash
+#psql -h db -U fms fms < /fixmystreet/db/schema.sql
+#psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
+#psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
+
 # reports
 /fixmystreet/bin/update-all-reports
+
+script/fixmystreet_app_server.pl -d --fork
