94ad3b80616efcbeb9be24e5ac3f8e17ca3ac961
milovanderlinden <milo@dogodigi.net>
2015-12-04 12:45:35 +0100

    Using hash to escape sed for url's

--- run.sh
+++ run.sh
@@ -12,14 +12,15 @@ sed -i "s/__DB_PORT__/$PG_PORT/g" /fixmystreet/conf/general.yml
 sed -i "s/__DB_NAME__/$PG_DATABASE/g" /fixmystreet/conf/general.yml
 sed -i "s/__DB_USER__/$PG_USER/g" /fixmystreet/conf/general.yml
 sed -i "s/__DB_PASS__/$PG_PASSWORD/g" /fixmystreet/conf/general.yml
-sed -i "s/__BASE_URL__/$BASE_URL/g" /fixmystreet/conf/general.yml
+sed -i "s#__BASE_URL__#$BASE_URL#g" /fixmystreet/conf/general.yml
 sed -i "s/__EMAIL_DOMAIN__/$EMAIL_DOMAIN/g" /fixmystreet/conf/general.yml
 sed -i "s/__CONTACT_EMAIL__/$CONTACT_EMAIL/g" /fixmystreet/conf/general.yml
 sed -i "s/__CONTACT_NAME__/$CONTACT_NAME/g" /fixmystreet/conf/general.yml
 sed -i "s/__NOREPLY_EMAIL__/$NOREPLY_EMAIL/g" /fixmystreet/conf/general.yml
 sed -i "s/__SMTP_SMARTHOST__/$SMTP_SMARTHOST/g" /fixmystreet/conf/general.yml
-sed -i "s/__GAZE_URL__/$GAZE_URL/g" /fixmystreet/conf/general.yml
-
+sed -i "s#__GAZE_URL__#$GAZE_URL#g" /fixmystreet/conf/general.yml
+sed -i "s#__MAPIT_URL__#$MAPIT_URL#g" /fixmystreet/conf/general.yml
+MAPIT_URL
 # reports
 /fixmystreet/bin/update-all-reports
 # run
